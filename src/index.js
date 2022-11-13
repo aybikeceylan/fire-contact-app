1c3380d6585e81e55c43dfa46ea0adb1b4672958
aybike <aybikesayar@gmail.com>
2022-11-14 00:16:32 +0300

    style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './output.css';
-import 'tw-elements';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-toastify/dist/ReactToastify.css";
